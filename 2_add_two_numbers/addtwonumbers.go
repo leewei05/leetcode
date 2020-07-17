e4049af52b9d2f9940a355efff5619080c16d51b
leewei05 <B10202013@mail.ntust.edu.tw>
2020-07-17 08:42:26 +0800

    2 addtwonumbers

---
+++ 2_add_two_numbers/addtwonumbers.go
@@ -0,0 +1,33 @@
+package addtwonumbers
+
+// O(max(m,n)) m for l1 length, n for l2 length
+// O(max(m,n)) +1
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    res := &ListNode{}
+    first := res
+    carry := 0
+    for l1 != nil || l2 !=nil || carry >0 {
+        sum := carry 
+        
+        if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+        
+        carry = sum / 10
+        
+        res.Next = &ListNode{sum%10,nil}
+        res = res.Next
+    }
+    
+    if carry > 0 {
+        res.Next = &ListNode{carry,nil}
+    }
+    
+    return first.Next
+}
