558b084b8548b91a26279180bb7a088990409154
leewei05 <B10202013@mail.ntust.edu.tw>
2020-07-18 11:37:31 +0800

    1480 run 1d

--- 2_add_two_numbers/addtwonumbers.go
+++ 2_add_two_numbers/addtwonumbers.go
@@ -1,15 +1,21 @@
 package addtwonumbers
 
+// ListNode is a struct
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 // O(max(m,n)) m for l1 length, n for l2 length
 // O(max(m,n)) +1
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-    res := &ListNode{}
-    first := res
-    carry := 0
-    for l1 != nil || l2 !=nil || carry >0 {
-        sum := carry 
-        
-        if l1 != nil {
+	res := &ListNode{}
+	first := res
+	carry := 0
+	for l1 != nil || l2 != nil || carry > 0 {
+		sum := carry
+
+		if l1 != nil {
 			sum += l1.Val
 			l1 = l1.Next
 		}
@@ -18,16 +24,16 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			sum += l2.Val
 			l2 = l2.Next
 		}
-        
-        carry = sum / 10
-        
-        res.Next = &ListNode{sum%10,nil}
-        res = res.Next
-    }
-    
-    if carry > 0 {
-        res.Next = &ListNode{carry,nil}
-    }
-    
-    return first.Next
+
+		carry = sum / 10
+
+		res.Next = &ListNode{sum % 10, nil}
+		res = res.Next
+	}
+
+	if carry > 0 {
+		res.Next = &ListNode{carry, nil}
+	}
+
+	return first.Next
 }
